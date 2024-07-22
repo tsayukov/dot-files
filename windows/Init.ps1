7853c9ec29334ea4565da582d9fc5ece9f7c600b
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-07-22 14:33:33 +0300

    Replace Write-Output to Print

--- windows/Init.ps1
+++ windows/Init.ps1
@@ -1,7 +1,8 @@
 #Requires -Version 7
 # Initialization after a fresh Windows installation
 
-. .\init_helpers.ps1
+# Include helper functions and variables
+. (Join-Path "$PSScriptRoot" "init_helpers.ps1")
 
 AppendToPSProfile @(
     '#####################################################'
@@ -18,32 +19,32 @@ winget install WingetPathUpdater
 # A `sudo` equivalent for Windows
 # See: https://github.com/gerardog/gsudo
 FindCommandOr "gsudo" {
-    Write-Output "Install gsudo..."
+    Print "Installing gsudo..."
     winget install --exact --id gerardog.gsudo
 }
 
 # A file archiver
 # See: https://www.7-zip.org/
 FindCommandOr "7z" {
-    Write-Output "Install 7-Zip..."
+    Print "Installing 7-Zip..."
     winget install --exact --id 7zip.7zip
     AddToEnvPath (Join-Path "$ENV:ProgramFiles" "7-Zip")
 }
 
 # A text editor, mostly for git
 FindCommandOr "vim" {
-    Write-Output "Install vim..."
-    Write-Output "NOTE: check the box 'Create .bat files' for command line use."
+    Print "Installing vim..."
+    Print "NOTE: check the box 'Create .bat files' for command line use."
     winget install --exact --id vim.vim --interactive
 }
 
 # A version control system
 # See: https://git-scm.com/download/win
 FindCommandOr "git" {
-    Write-Output "Install git..."
+    Print "Installing git..."
     winget install --exact --id Git.Git --interactive
 
-    Write-Output "Setting up user git configuration..."
+    Print "Setting up user git configuration..."
     Copy-Item -Path (Join-Path ".." "common" ".gitconfig") -Destination "$HOME"
     git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
     # TODO: ssh configuration
@@ -61,23 +62,26 @@ FindFontOr "JetBrainsMono NF" {
 
     # TODO: install fonts
 
+    Print "Removing $downloadedFilePath..."
     Remove-Item "$downloadedFilePath" -Force
+
+    Print "Removing $extractedDirPath..."
     Remove-Item "$extractedDirPath" -Force -Recurse
 }
 
 # Starship cross-shell prompt
 # See: https://starship.rs/
 FindCommandOr "starship" {
-    Write-Output "Install starship..."
+    Print "Installing starship..."
     winget install --exact --id Starship.Starship
 
-    Write-Output "Setting up starship configuration..."
+    Print "Setting up starship configuration..."
     $file = "starship.toml"
     $dir = ".config"
     $dirPath = MakeDirIfDoesNotExist (Join-Path "$HOME" "$dir")
     Copy-Item -Path (Join-Path ".." "common" "$file") -Destination "$filePath"
 
-    Write-Output "Append starship runner to PowerShell user profile..."
+    Print "Appending starship runner to PowerShell user profile..."
     $sep = [IO.Path]::DirectorySeparatorChar
     AppendToPSProfile @(
         '# Starship cross-shell prompt runner',
@@ -89,8 +93,8 @@ FindCommandOr "starship" {
 # A code editor
 # See: https://code.visualstudio.com/
 FindCommandOr "code" {
-    Write-Output "Install Microsoft Visual Studio Code..."
+    Print "Installing Microsoft Visual Studio Code..."
     winget install --exact --id Microsoft.VisualStudioCode --interactive
 }
 
-Write-Output "Please, re-run Terminal!"
+Print "Please, re-run Terminal!"
